2f1dc25f8cea3d0421c9af1c6e042db1fc08c596
Small-Belly <1453819976@qq.com>
2020-03-09 00:59:54 +0800

    Home banner

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" ref="mySwiper">
+      <swiper-slide class="swiper-item" v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+        loop: true, //支持循环轮播
+        autoplay: 2000
+      },
+      swiperList: [
+        //循环播放
+        {
+          id: "0001",
+          imgUrl: require("@/assets/img/banner_1.png")
+        },
+        {
+          id: "0002",
+          imgUrl: require("@/assets/img/banner_2.png")
+        },
+        {
+          id: "0003",
+          imgUrl: require("@/assets/img/banner_3.png")
+        }
+      ]
+    };
+  }
+};
+</script>
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active {
+  background: #fff !important;
+}
+
+.wrapper {
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 33.85%; // height/width
+  background: #ccc;
+
+  .swiper-item {
+    width: 100%;
+  }
+
+  .swiper-img {
+    width: 100%;
+  }
+}
+</style>
