e4d42493d5c499d3680f66d98ef0030f5cc28b28
Small-Belly <1453819976@qq.com>
2020-03-10 10:17:54 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="icons">
+    <swiper :options="swiperOption" ref="mySwiper">
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" />
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: "HomeIcons",
+  data() {
+    return {
+      swiperOption: {
+        //允许滑动
+        // autoplay: true
+      },
+      iconList: [
+        {
+          id: "0001",
+          imgUrl: require("img/piao.png"),
+          desc: "景点门票"
+        },
+        {
+          id: "0002",
+          imgUrl: require("img/hotel.png"),
+          desc: "酒店"
+        },
+        {
+          id: "0003",
+          imgUrl: require("img/flight.png"),
+          desc: "机票"
+        },
+        {
+          id: "0004",
+          imgUrl: require("img/package.png"),
+          desc: "度假"
+        },
+        {
+          id: "0005",
+          imgUrl: require("img/train.png"),
+          desc: "火车票"
+        },
+
+        {
+          id: "0006",
+          imgUrl: require("img/hotel.png"),
+          desc: "酒店"
+        },
+        {
+          id: "0007",
+          imgUrl: require("img/flight.png"),
+          desc: "机票"
+        },
+        {
+          id: "0008",
+          imgUrl: require("img/package.png"),
+          desc: "度假"
+        },
+        {
+          id: "0009",
+          imgUrl: require("img/train.png"),
+          desc: "火车票"
+        },
+        {
+          id: "00010",
+          imgUrl: require("img/piao.png"),
+          desc: "景点门票"
+        }
+      ]
+    };
+  },
+  //计算属性
+  computed: {
+    pages() {
+      const pages = [];
+      this.iconList.forEach((item, index) => {
+        //页码,向下取
+        const page = Math.floor(index / 8);
+        if (!pages[page]) {
+          pages[page] = [];
+        }
+        pages[page].push(item);
+      });
+      return pages;
+    }
+  }
+};
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+@import '~styles/mixins.styl';
+
+.icons >>> .swiper-container {
+  height: 0;
+  padding-bottom: 50%;
+}
+
+.icon {
+  float: left;
+  width: 25%;
+  height: 0;
+  position: relative;
+  padding-bottom: 25%;
+
+  .icon-img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0.44rem; // 22px
+    box-sizing: border-box;
+    padding: 0.1rem;
+
+    .icon-img-content {
+      display: block;
+      margin: 0 auto;
+      height: 100%;
+    }
+  }
+
+  .icon-desc {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 0.44rem;
+    text-align: center;
+    line-height: 0.44rem;
+    color: $darkTextColor;
+    ellipsis();
+  }
+}
+</style>
