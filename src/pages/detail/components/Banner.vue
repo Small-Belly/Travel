aa5890dedfc10f24b7f99dfc134971c9e9a79b0a
Small-Belly <1453819976@qq.com>
2020-03-15 19:45:41 +0800

    header animation

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <div class="banner" @click="handleBannerClick">
-      <img
-        class="banner-img"
-        src="https://img1.qunarzz.com/vs_ceph_vs_tts/313147f7-677f-44bf-a091-6999b113e76e.jpg_r_640x420x95_ff0fb8f6.jpg"
-      />
+      <swiper :options="swiperOption">
+        <swiper-slide v-for="(item, index) in imgs" :key="index">
+          <img class="banner-img" :src="item" alt />
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
       <div class="txt">
         <div class="id">
           <span class="tit">产品编号</span>
@@ -27,16 +29,28 @@
 import CommonGallary from "common/gallary/Gallary";
 export default {
   name: "DetailBanner",
-
   components: {
     CommonGallary
   },
   data() {
     return {
       showGallary: false,
+      swiperOption: {
+        loop: true,
+        autoplay: 3000,
+        pagination: ".swiper-pagination",
+        paginationType: "fraction",
+        //监听变化,自动刷新
+        observeParents: true,
+        observer: true
+      },
       imgs: [
         "https://img1.qunarzz.com/vs_ceph_vs_tts/313147f7-677f-44bf-a091-6999b113e76e.jpg_r_1280x840x95_a34352bf.jpg",
-        "https://img1.qunarzz.com/vs_ceph_vs_tts/73f93f46-49a2-484b-993c-3d128256e5bb.jpg_r_1280x840x95_a8fed6b2.jpg"
+        "https://img1.qunarzz.com/vs_ceph_vs_tts/73f93f46-49a2-484b-993c-3d128256e5bb.jpg_r_1280x840x95_a8fed6b2.jpg",
+        "https://img1.qunarzz.com/vs_ceph_vs_tts/700ef324-e4a4-431f-9c07-f32476c8e631.jpg_r_640x420x95_70a8b25a.jpg",
+        "https://img1.qunarzz.com/vs_ceph_vs_tts/8177916d-9b0a-4da7-b5d6-f2a5077ea78e.jpg_r_640x420x95_fd2407e7.jpg",
+        "https://img1.qunarzz.com/vs_ceph_vs_tts/bf0e6371-7085-406f-9ea5-d215f64eba99.jpg_r_640x420x95_2db32056.jpg",
+        "https://img1.qunarzz.com/p/tts5/1705/46/062b56ad1351a202.jpg_r_640x420x95_95979774.jpg"
       ]
     };
   },
@@ -51,6 +65,9 @@ export default {
 };
 </script>
 <style lang="stylus" scoped>
+.banner >>> .swiper-container {
+    overflow:inherit;
+}
 .banner{
   position:relative;
   overflow:hidden;
@@ -91,6 +108,10 @@ export default {
       }
     }
   }
-
+  // .swiper-container{
+  //   display:inline-block;
+  //   bottom:0;
+  //   color:#fff;
+  // }
 }
 </style>
---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="header">
+    <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
+      <div class="iconfont header-abs-back">&#xe624;</div>
+    </router-link>
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+      <router-link to="/">
+        <div class="header-left">
+          <div class="iconfont header-fixed-back">&#xe624;</div>
+        </div>
+      </router-link>
+      <div class="header-right">景点选择</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "DetallHeader",
+  data() {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    };
+  },
+  methods: {
+    handleScroll() {
+      const top = document.documentElement.scrollTop;
+      if (top > 60) {
+        let opacity = top / 140;
+        opacity = opacity > 1 ? 1 : opacity;
+        this.opacityStyle = {
+          opacity
+        };
+        this.showAbs = false;
+      } else {
+        this.showAbs = true;
+      }
+    }
+  },
+  activated() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+};
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles';
+
+.header {
+  .header-abs {
+    position: absolute;
+    top: 0.1rem;
+    left: 0.1rem;
+    width: 0.72rem;
+    height: 0.72rem;
+    border-radius: 50%;
+    line-height: 0.72rem;
+    background: rgba(0, 0, 0, 0.6);
+    z-index: 100;
+    text-align: center;
+    .header-abs-back{
+      color: #fff;
+      font-size: 0.4rem;
+    }
+  }
+
+  .header-fixed {
+      position:fixed;
+      top:0;
+      left:0;
+      right:0;
+      height: $headerHight;
+      line-height: $headerHight;
+      background: $bgColor;
+      color: #fff;
+      z-index:100;
+
+    .header-left {
+      width: 0.64rem;
+      position: absolute;
+      top: 0;
+      left: .1rem;
+
+      .header-fixed-back {
+        text-align: center;
+        font-size: 0.4rem;
+        color: #fff;
+      }
+    }
+
+    .header-right {
+      text-align: center;
+    }
+  }
+}
+</style>
